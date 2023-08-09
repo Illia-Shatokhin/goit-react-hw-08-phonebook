2e86b8b9a1a5c4fd7d1dd2e1796461241426a740
Illia Shatokhin <resonane.2106@gmail.com>
2023-08-09 21:20:39 +0300

    add started files and routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+import { SharedLayout } from './SharedLayout/SharedLayout';
+import { lazy } from 'react';
+
+const Home = lazy(() => import('pages/Home/Home'));
+const Register = lazy(() => import('pages/Register/Register'));
+const Login = lazy(() => import('pages/Login/Login'));
+const Contacts = lazy(() => import('pages/Contacts/Contacts'));
+const NotFound = lazy(() => import('pages/NotFound/NotFound'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/register">Register</NavLink>
+        <NavLink to="/login">Login</NavLink>
+        <NavLink to="/contacts">Contacts</NavLink>
+        <NavLink to="*">NotFound</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
